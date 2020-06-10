ac8a486c1fc68fa97d2b9c582de2a52ddfb55a4d
Connor Horman <chorman64@gmail.com>
2020-06-10 17:17:56 -0400

    Poke some stuff

--- include/qc_int.h
+++ include/qc_int.h
@@ -7,6 +7,8 @@
 
 #include <qc_base.h>
 
+/// Provides declarations for all qc operations on integer types
+
 #define __qc_int_ops(type)\
     __qc_def_bop(type,add,+);\
     __qc_def_bop(type,sub,-);\
@@ -36,5 +38,16 @@
     __qc_int_ops(ualias)
 
 #include "qc_int.inc"
+
+// Define Macros for each of the qc operators
+// Standard methods of macro function supression are allowed
+#define __qc_op_add_Int8(a,b) ((_Int8)((_Int8)a+(_Int8)b)))
+#define __qc_op_add_UInt8(a,b) ((_UInt8)((_UInt8)a+(_UInt8)b)))
+#define __qc_op_add_Int16(a,b) ((_Int16)((_Int16)a+(_Int16)b)))
+#define __qc_op_add_UInt16(a,b) ((_UInt16)((_UInt16)a+(_UInt16)b)))
+#define __qc_op_add_Int32(a,b) ((_Int32)((_Int32)a+(_Int32)b)))
+#define __qc_op_add_UInt32(a,b) ((_UInt32)((_UInt32)a+(_UInt32)b)))
+#define __qc_op_add_Int64(a,b) ((_Int64)((_Int64)a+(_Int64)b)))
+#define __qc_op_add_UInt64(a,b) ((_UInt64)((_UInt64)a+(_UInt64)b)))
 
 #endif //SNES_DEV_QC_INT_H
