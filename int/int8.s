4a559d046927a433129da248940c8a4ad9e12bd3
Connor <chorman64@gmail.com>
2021-08-04 21:23:56 -0400

    Start writing qc

---
+++ int/int8.s
@@ -0,0 +1,18 @@
+
+__fixed_add8:
+__uint_add8:
+__int_add8:
+    txa
+    clc
+    sty __r1,%D
+    adc __r1,%D
+    rtl
+
+__fixed_sub8:
+__uint_sub8:
+__int_sub8:
+    txa
+    clc
+    sty __r1,%D
+    sbc __r1,%D
+    rtl
