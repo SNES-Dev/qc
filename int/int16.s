e39fc8bd73307e06b57f5e3ac41ff4ca1ebc533d
Connor Horman <chorman64@gmail.com>
2021-08-04 22:54:16 -0400

    Add more files

--- int/int16.s
+++ int/int16.s
@@ -1,18 +1,22 @@
 
-__fixed_add8:
-__uint_add8:
-__int_add8:
+__fixed_add16:
+__uint_add16:
+__int_add16:
+    rep #$10
     txa
     clc
     sty __r1,%D
     adc __r1,%D
+    sep #$20
     rtl
 
-__fixed_sub8:
-__uint_sub8:
-__int_sub8:
+__fixed_sub16:
+__uint_sub16:
+__int_sub16:
+    rep #$10
     txa
     clc
     sty __r1,%D
     sbc __r1,%D
+    sep #$20
     rtl
